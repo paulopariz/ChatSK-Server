af8d160bf1788121520713c2a05b702bddd894c1
paulopariz <paulopariz01@gmail.com>
2024-01-14 21:01:03 -0300

    feat - connect user in room

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,29 @@
 import { io } from "./http";
 
+interface RoomUser {
+  socket_id: string;
+  username: string;
+  room: string;
+}
+
+const users: RoomUser[] = [];
+
 io.on("connection", (socket) => {
-  console.log("Client - ", socket.id);
+  socket.on("select_room", (data) => {
+    socket.join(data.room);
+
+    const userInRoom = users.find(
+      (user) => user.username === data.username && user.room === data.room
+    );
+
+    if (userInRoom) {
+      userInRoom.socket_id = socket.id;
+    } else {
+      users.push({
+        room: data.room,
+        username: data.username,
+        socket_id: socket.id,
+      });
+    }
+  });
 });
